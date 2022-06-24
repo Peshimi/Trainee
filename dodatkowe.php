a80308c437b02631dd3a7d9f6f1d21a5091976ea
Nadia Lazarek <woj.dag@o2.pl>
2022-06-24 22:21:57 +0200

    fixed white space and date format

--- dodatkowe.php
+++ dodatkowe.php
@@ -3,40 +3,45 @@ $handle = fopen("zadanko.csv", "r");
 $a = array();
   if ($handle) { 
     while (($line = fgets($handle)) !== false) {
-     #echo $line;
+    #echo $line;
 
-    $exp = substr($line , strpos($line, ',')+1); # explode date in format YYYY-MM-DD
-                                                 # into array of array[0] - YEAR
-                                                 # array[1] - MONTH
-                                                 # array[2] - DAY
+    $all_dates = substr($line , strpos($line, ',')+1);                                             
     $start_date = "2000-01-01";
 
-    if ($start_date<$exp){
+    if ($start_date<$all_dates){
+           
+    #$tmp = explode("-", $all_dates);              #explode date in format YYYY-MM-DD into array
+    #$dates = "$tmp[2].$tmp[1].$tmp[0]";           #array[0] - YEAR | array[1] - MONTH | array[2] - DAY
+    #echo $dates;     
+   
+    $dates = trim(preg_replace('/\s+/', '', $all_dates));   #remove new line from string *white space*
+    $data = date_format(date_create($dates), "d.m.Y");
 
-    $tmp = explode("-", $exp);
-    $date = "$tmp[2].$tmp[1].$tmp[0]";
-     #echo $date;
-     var_dumb($tmp);
-
-       if (empty($a[$date])) { 
-         $a += array($date=>1);
+       if (empty($a[$data])) { 
+         $a += array($data=>1);
        }
        else {
-           $a[$date]+=1;
+           $a[$data]+=1;
        }
     }
   }
-   fclose($handle);
+  fclose($handle);
  }
+  #var_dump($tmp);
+  #var_dump(array("2001","01","02"));
 
    arsort($a);
 
    $counter = 0;
    foreach($a as $x => $x_value){
      if($counter<10){
-       echo "Data: " . $x . " & " . "Ilosc: " . $x_value . ";\n";
+      #echo date($x) . " & liczba powtórzeń: " . $x_value . ";\n"; 
+      echo "Data: " . $x . " & " . "Ilosc: " . $x_value . ";\n";
    }
    $counter++;
  }
 
-#str_replace('-', '.', $x) #line 32# works but prints dates by year/month/day, I need day/month/year
+
+#str_replace('-', '.', $x) works but prints dates by year/month/day, I need day/month/year
+
+?>
